d08b44b63c72196f227adfc5b2cf27203f6f2708
Hoang Hai Ngo <hoanghai.ngo@elektrobit.com>
2019-12-12 17:10:37 +0100

    Delete uncessary classes

--- app/src/main/java/com/example/spritpreise/model/StationWrapper.kt
+++ app/src/main/java/com/example/spritpreise/model/StationWrapper.kt
@@ -3,7 +3,6 @@ package com.example.spritpreise.model
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-//@JsonClass(generateAdapter = true)
 data class StationWrapper(
 
     @Json(name = "ok")
